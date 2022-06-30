0ea6d10d26868274a091e61531afe110b3e6f1fc
unknown <sofiabernabeicejas@gmail.com>
2022-06-30 11:18:59 -0300

    vamos de nuevo

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ ReactDOM.render(
 
 // prueba numero dos, muchos commts
 //seunda prueba de muchos commit e intentar hacer comentarios en los commit
+//nfksdnfknfksdfnsdf
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
